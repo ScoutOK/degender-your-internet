cf3aa13c8108a136cafb89bc3340b2ecd5cf527d
ScoutOK <ohkeefe@gmail.com>
2016-11-10 23:53:17 -0500

    this branch got out of hand, just committing it anyway

--- app/main.jsx
+++ app/main.jsx
@@ -1,15 +1,25 @@
 import React from 'react'
 import { render } from 'react-dom'
 import Container from './components/Container'
+import Popup from './components/Popup'
 import { Router, Route, IndexRedirect, browserHistory } from 'react-router'
+import * as firebase from 'firebase'
+
+const config = {
+    apiKey: "AIzaSyDhVC1K39EUPWBekzHXgltSe1xkQR6US-c",
+    authDomain: "degendertheinternet.firebaseapp.com",
+    databaseURL: "https://degendertheinternet.firebaseio.com",
+    storageBucket: "degendertheinternet.appspot.com",
+    messagingSenderId: "919863122992"
+};
 
 
+firebase.initializeApp(config);
+
+window.React = React;
 
 render (
-          <Router history={browserHistory}>
-            <Route path="/" component={Container} >
-            </Route>
-          </Router>
+          <Container />
         ,
   document.getElementById('app')
 )
--- webpack.config.js
+++ webpack.config.js
@@ -17,7 +17,7 @@ module.exports = {
     loaders: [
       {
         test: /jsx?$/,
-        exclude: /(node_modules|bower_components)/,
+        exclude: /node_modules/,
         loader: 'babel',
         query: {
           presets: ['react', 'es2015', 'stage-2']
