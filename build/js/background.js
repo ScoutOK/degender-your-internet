f455c3765b00931f1f022835504d6820009c2b11
ScoutOK <ohkeefe@gmail.com>
2017-03-20 18:59:11 -0400

    slight refactoring closes #72

--- build/js/background.js
+++ build/js/background.js
@@ -12,25 +12,24 @@ webpackJsonp([1,4],[
 	  adjectives: {}
 	};
 	
-	// const syncTabQuery = Promise.promisify(chrome.tabs.query())
+	const tabProm = () => {
+		return new Promise(function (resolve, reject) {
+			chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
+				if (Array.isArray(tabs)) {
+					resolve(tabs);
+				} else {
+					reject('not in a tab');
+				}
+			});
+		});
+	};
 	
 	chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
 	  console.log(sender.tab ? "from a content script:" + sender.tab.url : "from the extension");
 	  if (request.message == "analyze") {
 	    var tabIdx = void 0;
-	    var tabProm = function tabProm() {
-	      return new Promise(function (resolve, reject) {
-	        chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
-	          if (Array.isArray(tabs)) {
-	            resolve(tabs);
-	          } else {
-	            reject('not in a tab');
-	          }
-	        });
-	      });
-	    };
 	
-	    tabProm().then(function (tabs) {
+	    tabProm().then((tabs) => {
 	      tabIdx = tabs[0].index + 1;
 	      chrome.tabs.create({
 	        url: analyticsURL,
@@ -39,11 +38,7 @@ webpackJsonp([1,4],[
 	        console.log('well i tried');
 	      });
 	    }).catch(console.error);
-	    // chrome.tabs.query({currentWindow: true, active: true}, function (tabs){
-	    //   console.log('current tab index', tabs[0].index)
-	    //   tabIdx = tabs[0].index + 1;
-	    // });
-	    //console.log('the analysis tab id', tabIdx);
+
 	    sendResponse({ farewell: "we made it this far" });
 	    var analyticsURL = chrome.extension.getURL('analytics.html');
 	  }
